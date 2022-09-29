992b919db3dd9a858fdea3747f1f1d0228acb95b
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-29 11:04:32 +0300

    commit tree 19cf056bbd2422031ecb5da9011b9420a312071a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    # validates :must_genre
+    validates :content, length: {minimum: 100}
+
+    # def must_genre
+    #     if content = Fiction
+    #          true
+     
+    #         elsif content = Non-Fiction
+    #             true
+          
+    #             else  
+    #                 errors.add(:email, "No!")
+    #             end
+    #         end
+    #     end
+        
+end 
+    
+
